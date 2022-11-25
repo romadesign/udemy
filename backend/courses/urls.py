cde20dfeb0e010546dc9b5dd37999e58aa0e505f
romadesign <roman247.rg@gmail.com>
2022-11-25 11:46:32 +0100

    function: add coment

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import Create_Course, ListCoursesView
+from .views import Create_Course, ListCoursesView, Course_Detail, Add_Comment_View
 
 urlpatterns = [
     path('register/course', Create_Course.as_view()),
     path('get-courses', ListCoursesView.as_view()),
+    path('course/<int:id>/', Course_Detail.as_view()),
+    path('comment/<int:course_id>', Add_Comment_View.as_view()),
 ]
--- backend/courses/views.py
+++ backend/courses/views.py
@@ -3,9 +3,10 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .serializers import get_Courses_Serializer
+from .serializers import get_Courses_Serializer, Comment_Serializer
 from .models import Course, Comment
 from users.models import User
+import json
 
 # Create your views here.
 # class Create_Course(APIView):
@@ -20,24 +21,24 @@ from users.models import User
 #             'message': 'User registered  successfully',
 #             }
 #         return Response(response, status=status_code)
-    
+
 
 class Create_Course(APIView):
-    def post(self, request, *args, **kwargs) :
+    def post(self, request, *args, **kwargs):
         # # validation
         serializer = get_Courses_Serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
-        data = self.request.data 
-        
-        author=data['author']
-        price=data['price']
-        
+
+        data = self.request.data
+
+        author = data['author']
+        price = data['price']
+
         if price.find(".") == -1:
             price = price + ".0"
 
         author = get_object_or_404(User, id=author)
-        
+
         course = Course(
             title=data['title'],
             description=data['description'],
@@ -45,19 +46,18 @@ class Create_Course(APIView):
             author=author,
             payment=data['payment'],
             price=price
-            )
-        
-        course.save()            
-            
+        )
+
+        course.save()
+
         # comments_data=data['comments']
         # for comment in comments_data:
         #     comments = Comment(user = comment['user'],message = comment['message'])
         #     course.comments.add(comments)
-        
-        
+
         return Response({'success': 'Message sent successfully'})
-    
-    
+
+
 class ListCoursesView(APIView):
     def get(self, request):
 
@@ -77,8 +77,7 @@ class ListCoursesView(APIView):
         elif order == 'asc':
             courses = Course.objects.order_by(sortBy).all()[:int(limit)]
         else:
-            courses = Course.objects.order_by(sortBy).all() 
-            
+            courses = Course.objects.order_by(sortBy).all()
 
         courses = get_Courses_Serializer(courses, many=True)
 
@@ -86,3 +85,66 @@ class ListCoursesView(APIView):
             return Response({'courses': courses.data}, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'No courses to list'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class Course_Detail(APIView):
+    def get(self, request, *args, **kwargs):
+        id = self.kwargs['id']
+        course_exists = Course.objects.filter(id=id)
+
+        course_exists.exists()
+        if course_exists:
+            course = course_exists.first()
+            serializer = get_Courses_Serializer(course)
+
+            return Response({
+                'success': 'true',
+                'status code': status.HTTP_201_CREATED,
+                "course": serializer.data
+            })
+        else:
+            return Response({
+                'success': 'true',
+                'status code': status.HTTP_404_NOT_FOUND,
+                "course": "Not exists"
+            })
+
+
+class Add_Comment_View(APIView):
+    def post(self, request, course_id, *args, **kwargs):
+        # course_id = self.kwargs['course_id'] or
+
+        try:
+            course = Course.objects.get(id=course_id)
+        except Course.DoesNotExist:
+            return Response({
+                'success': 'false',
+                'status code': status.HTTP_400_BAD_REQUEST,
+                "message": "Course not exists"
+            })
+
+        # capture the data sent - frontend
+        content = json.loads(request.body)
+
+        if not content.get('message'):
+            return Response({
+                'success': 'false',
+                'status code': status.HTTP_400_BAD_REQUEST,
+                "message": "message is empty "
+            })
+
+        serializer = Comment_Serializer(data=content)
+
+        if serializer.is_valid(raise_exception=True):
+            comment = serializer.save(user=self.request.data['user'])
+
+            course.comments.add(comment)
+
+            return Response({
+                'success': 'true',
+                'status code': status.HTTP_200_OK,
+                "message": "comment added successfully"
+            })
+
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
