636b33856207c558bb1a4594dff442b5c2803cde
romadesign <roman247.rgggmail.com>
2022-12-03 20:24:04 +0100

    function: create random data with faker

--- backend/category/models.py
+++ backend/category/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
@@ -8,16 +10,17 @@ class Category(models.Model):
 
     parent = models.ForeignKey(
         'self', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
-    
+
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class Comment(models.Model):
-    user =              models.CharField(max_length=255)
-    message =           models.TextField()
-    created =           models.DateTimeField(auto_now_add=True)
-    
+    user = models.CharField(max_length=255)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.message
---
+++ backend/course_test.py
@@ -0,0 +1,74 @@
+from category.models import Category
+from users.models import User
+from courses.models import Course
+from faker import Faker
+import random
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+django.setup()
+
+fake = Faker()
+
+
+categories = [
+    "Javascript",
+    "Python",
+    "CC+",
+    "Angular",
+    "Php",
+    "Html5",
+    "React Js",
+    "Vue JS",
+    "Desarrollo web",
+    "Backend",
+    "Frontend",
+    "Full Stack",
+    "Laravel",
+    "Next Js"
+]
+
+data = ""
+
+
+def categories_test(data):
+    for dia in categories:
+        name = dia
+        obj = Category.objects.get_or_create(
+            name=name,
+        )
+
+
+def course_test(value):
+    for _i in range(value):
+        author = User.objects.get(id=random.randint(1,2))
+        title = fake.text(max_nb_chars=65)
+        description = fake.text()
+        language = "espanol"
+        payment = "free"
+        price = random.randint(1,20)
+        status = "published"
+        category = Category.objects.get(id=random.randint(1,2))
+        obj = Course.objects.get_or_create(
+            author = author,
+            title = title,
+            description = description,
+            language = language,
+            payment = payment,
+            price = price,
+            status = status,
+            category = category
+        )
+
+def main():
+    no = int(input("how many recors you want to send"))
+    categories_test(no)
+    course_test(no)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# create data
+# python course__test.py
