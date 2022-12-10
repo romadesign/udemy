d6e6900805d2d0d1ce6c727a0a00648374785146
romadesign <roman247.rg@gmail.com>
2022-12-10 14:17:56 +0100

    add options image test

--- backend/course_test.py
+++ backend/course_test.py
@@ -1,14 +1,14 @@
-from category.models import Category
-from users.models import User
-from courses.models import Course, WhatLearnt
+# from category.models import Category
+# from users.models import User
+# from courses.models import Course, WhatLearnt
 from faker import Faker
 import random
 import os
 import django
 from django.shortcuts import get_object_or_404
-
+import tempfile
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-django.setup()
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 fake = Faker()
 
@@ -46,15 +46,17 @@ def course_test(value):
         author = User.objects.get(id=random.randint(1, 2))
         title = fake.text(max_nb_chars=65)
         description = fake.text()
+        image = 'courses/romacode/f976e25d-e572-49c4-b5f4-cc0752469409.png'
         language = "espanol"
         payment = "free"
         price = random.randint(1, 20)
         status = "published"
-        category = Category.objects.get(id=random.randint(1, 2))
+        category = Category.objects.get(id=random.randint(1, 14))
         obj = Course.objects.get_or_create(
             author=author,
             title=title,
             description=description,
+            image=image,
             language=language,
             payment=payment,
             price=price,
@@ -65,13 +67,20 @@ def course_test(value):
 
 def main():
     no = int(input("how many recors you want to send"))
-    # categories_test(no)
-    # course_test(no)
+    #categories_test(no)
+    course_test(no)
 
 
 if __name__ == "__main__":
+    django.setup()
+    from category.models import Category
+    from users.models import User
+    from courses.models import Course, WhatLearnt
+ 
     main()
 
 
 # create data
 # python course__test.py
+# https://stackoverflow.com/questions/48163641/django-core-exceptions-appregistrynotready-apps-arent-loaded-yet-django-2-0
+# https://www.youtube.com/watch?v=c9FPZiOtXVU&t=769s
