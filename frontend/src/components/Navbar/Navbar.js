4920bd17ac013c0061e63d6a5a53a09588d741e8
romadesign <roman247.rg@gmail.com>
2023-01-18 13:50:32 +0100

    add styles navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -6,13 +6,17 @@ const Navbar = () => (
       <div className={styles.content_one}>
         <img width={100} src='/img/logo-udemy.svg' />
         <span>Categories</span>
-        <input type='search' placeholder='Search' />
+        <input
+          type='search'
+          placeholder='	
+&#128269; Search'
+        />
       </div>
       <div className={styles.content_two}>
         <span>teach on udemy</span>
-        <img width={30} src='/img/carrito.svg' />
-        <button>Login</button>
-        <button>Register</button>
+        <img width={25} src='/img/carrito.svg' />
+        <button className={styles.login}>Log in</button>
+        <button className={styles.register}>Sign up</button>
       </div>
     </div>
   </div>
