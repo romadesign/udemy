cb87d2bcb5cda92fdd1856507a36e2594d8ccb1f
romadesign <roman247.rg@gmail.com>
2023-02-13 12:17:52 +0100

    create function frontend: select couse by category

--- frontend/src/components/cardUser/CardCourse.js
+++ frontend/src/components/cardUser/CardCourse.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useMemo } from 'react'
 import Card from '../GeneralCardComponent/card'
 import Pagination from '../Pagination/Pagination'
 import { useRouter } from 'next/router'
@@ -8,33 +8,36 @@ import styles from '@/styles/course.module.css'
 const CardCourse = ({ data, user }) => {
   const slider = useRef()
   const router = useRouter()
-  const [courses, setCourse] = useState()
+  const [courses, setCourses] = useState([])
   const [next, setNext] = useState()
   const [previous, setPrevious] = useState()
-
+  const [categoryList, setCategoryList] = useState([])
   const [payload, setPayload] = useState({
     p: 1,
     page_size: 8,
     sort: 'id',
-    category: ""
-  }) 
-
+    category: ''
+  })
 
   useEffect(() => {
     getData()
-  }, [])
+  }, [payload])
 
   const getData = async () => {
     data(user, payload)
-      .then(function (res) {
-        setCourse(res.results.data)
+      .then(res => {
+        setCourses(res.results.data)
         setNext(res.next)
         setPrevious(res.previous)
-        console.log(res.results.categoryList)
+        setCategoryList(res.results.categoryList)
       })
-      .catch(function (error) {
-        // console.log(error)
+      .catch(error => {
+        console.error(error)
       })
+  }
+  const onchangeSelectCategory = e => {
+    //change value payload.category
+    setPayload({ ...payload, category: e })
   }
 
   return (
@@ -54,27 +57,40 @@ const CardCourse = ({ data, user }) => {
           <div>
             <div>Filter by</div>
             <div>
-              <select>
-                <option>Categories</option>
-                <option>Title A-to Z</option>
-                <option>Title Z-to A</option>
+              <select
+                onChange={event => onchangeSelectCategory(event.target.value)}
+              >
+                <option value=''>Select option</option>
+
+                {categoryList !== undefined &&
+                  categoryList.map(item => (
+                    <option key={item.id} value={item.id}>
+                      {item.name}
+                    </option>
+                  ))}
               </select>
             </div>
           </div>
         </div>
       )}
-      <div className={router.pathname == '/' ? styles.button_left_rigth : styles.button_left_rigth_learning_wishlist}>
+      <div
+        className={
+          router.pathname == '/'
+            ? styles.button_left_rigth
+            : styles.button_left_rigth_learning_wishlist
+        }
+      >
         <div ref={slider} className={styles.content}>
           {courses != undefined &&
             courses.map((course, id) => <Card key={id} course={course} />)}
         </div>
-          <Pagination 
-            next={next}
-            setNext={setNext}
-            previous={previous}
-            setPrevious={setPrevious}
-            setCourse={setCourse}
-          />
+        <Pagination
+          next={next}
+          setNext={setNext}
+          previous={previous}
+          setPrevious={setPrevious}
+          setCourse={setCourses}
+        />
       </div>
     </div>
   )
